8e1e5ce7260f5dd3a2df3b3099e8c85460112a6c
Sneguradik <93195734+Sneguradik@users.noreply.github.com>
2022-05-01 16:44:17 +0300

    Add files via upload

--- QuizBot/keyboards/admin.py
+++ QuizBot/keyboards/admin.py
@@ -1,3 +1,3 @@
 from keyboards.client import ComandKeyboard
 
-kbca = ComandKeyboard(True, '/AddQuestion', '/AllQuestions')
+kbca = ComandKeyboard(True, '/AddQuestion', '/AllQuestions', 'DelQuestion')
